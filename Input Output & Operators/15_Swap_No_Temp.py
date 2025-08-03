fb0759f5f6c1d8313f2da99cc721bd84dad93f9a
Suraj Aglave <aglavesuraj96@gmail.com>
2025-08-03 11:54:56 +0530

    Delete 15_Swap_No_Temp.py

--- Input Output & Operators/15_Swap_No_Temp.py
+++
@@ -1,8 +0,0 @@
-a = int(input("Enter the First number :"))
-b = int(input("Enter the Second number : "))
-
-print("Before Swap: a =", a, " b =", b)
-
-a,b = b,a
-
-print("After Swap: a =", a, " b =", b)
