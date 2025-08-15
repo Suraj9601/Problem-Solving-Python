a7fa18a1f710f6e5a26b0f7b37c7e5b5de5b0c13
Suraj Aglave <aglavesuraj96@gmail.com>
2025-08-15 17:42:01 +0530

    Delete round-value.py

--- Input Output & Operators/round-value.py
+++
@@ -1,6 +0,0 @@
-# 34. Input a float and print its rounded value.
-
-a = float(input("Enter the float number : "))
-print("Original Value :", a)
-rounded_value = round(a)
-print("Rounded Value : ", rounded_value)
