10ecf242e8b322cabf8b6b5271d0bd34f5e7f338
Suraj Aglave <aglavesuraj96@gmail.com>
2025-09-15 17:48:45 +0530

    Create totalTax.py

---
+++ Input Output & Operators/totalTax.py
@@ -0,0 +1,7 @@
+# 66. Calculate total bill with tax (price + tax%).
+price = float(input("Enter the price : "))
+tax = float(input("Enter the Tax in % : "))
+bill = price + tax
+print("Price : ", price)
+print("Tax : ", tax, "%")
+print("Total bill : ", bill)
